c5b520782ac243691e44a23d27f6cafa66bbcd75
Samuel Rats <samuel.rats@gmail.com>
2012-12-02 13:49:30 +0100

    Licensing Android App under Apache License

--- Back office/index.php
+++ Back office/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 1999-2004 The Apache Software Foundation
+ * Copyright 2012-2013 The Apache Software Foundation
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- *limitations under the License.
+ * limitations under the License.
 **/
 
 define('EXEC', 0xDEADBEEF);
